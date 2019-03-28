dfb072a3791f859da703ce484e364011630f642a
gobiyoga <gobi.yogarajah@uoit.net>
2019-03-27 23:09:51 -0400

    Update and rename main menu.html to index.php

---
+++ index.php
@@ -0,0 +1,107 @@
+<?php
+session_start();
+require 'includes/db.php';
+require 'includes/def.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>SudokuMAX - Home</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/heroic-features.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+
+</head>
+
+<body>
+
+<!-- Navigation -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <div class="container">
+        <a class="navbar-brand" href="/">SudokuMAX</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="/">Home
+                        <span class="sr-only">(current)</span>
+                    </a>
+                </li>
+                <?php
+                if ( isset( $_SESSION['user_id'] ) ) {
+                    echo '
+                    <li class="nav-item">
+                        <a class="nav-link" href="dashboard.php">Dashboard</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="logout.php">Logout (' . $_SESSION["user_id"] . ')</a>
+                    </li>
+                    ';
+                }
+                else {
+                    echo '
+                    <li class="nav-item">
+                        <a class="nav-link" href="register.php">Register</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="login.php">Login</a>
+                    </li>
+                    ';
+                }
+                ?>
+
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<!-- Page Content -->
+<div class="container">
+
+    <!-- Jumbotron Header -->
+    <header class="jumbotron my-4">
+        <h1 class="display-4">Welcome to SudokuMAX!</h1>
+        <p class="lead">Best online interactive Sudoku game!!!</p>
+        <?php
+        if ( isset( $_SESSION['user_id'] ) ) {
+            echo '<a href="dashboard.php" class="btn btn-primary btn-lg">Go To Dashboard!</a>';
+        }
+        else {
+            echo '<a href="register.php" class="btn btn-primary btn-lg">Register Now!</a>';
+        }
+        ?>
+    </header>
+
+</div>
+<!-- /.container -->
+
+<!-- Footer -->
+<footer class="footer bg-dark">
+    <div class="container">
+        <p class="m-0 text-center text-white">Copyright &copy; SudokuMAX 2019</p>
+    </div>
+    <!-- /.container -->
+</footer>
+
+<!-- Bootstrap core JavaScript -->
+<script src="js/jquery/jquery.min.js"></script>
+<script src="js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
